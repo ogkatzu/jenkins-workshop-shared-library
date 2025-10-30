5bebafa7d156a19baea0686d6efb452779894c75
skatz <saarkatz0913@gmail.com>
2025-10-30 11:22:02 +0200

    commit

--- vars/printEnvInfo.groovy
+++ vars/printEnvInfo.groovy
@@ -1,7 +1,7 @@
 // vars/printEnvInfo.groovy
 
 def call(config = [:]){
-    echo 'branch name ${env.BRANCH_NAME}'
-    echo 'build number ${env.BUILD_NUMBER}'
+    echo "branch name ${env.BRANCH_NAME}"
+    echo "build number ${env.BUILD_NUMBER}"
     echo "Jenkins URL ${env.JENKINS_URL}"
 }
